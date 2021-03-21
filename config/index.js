dbb980f43d6a934cb786146d77d00ddafbca070c
Benjamin <bcruz@asdeporte.com>
2021-03-21 01:47:51 -0600

    port change

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
   dev: process.env.NODE_ENV !== 'production',
   db: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TEST : process.env.MONGO_URI_PROD,
-  port: process.env.PORT || 8080
+  port: process.env.PORT || 3001
 };
 
 module.exports = config;
